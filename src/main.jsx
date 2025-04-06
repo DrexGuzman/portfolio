8dbbd5ce3a17c5003dea59eb7890bba62a91cb1a
DrexGuzman <drexjesusguzman@gmail.com>
2025-04-05 22:27:47 -0600

    _redirects file

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
+    <BrowserRouter>
         <App />
     </BrowserRouter>
   </>
